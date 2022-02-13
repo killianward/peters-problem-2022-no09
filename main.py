db430cdc07557adc2db046a7ec26eddf955954ad
WardCast <killianward@gmail.com>
2022-02-13 16:50:21 +0000

    fixed the program

--- main.py
+++ main.py
@@ -13,51 +13,41 @@ digit_to_list_dict = {
     9: [1, 1, 1, 1, 0, 1, 0]}
 
 four_zero_three = [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1]
-digits = [0, 1]
-indices_of_digit1 = [0, 1, 2, 3, 4, 5, 6]
-indices_of_digit2 = [7, 8, 9, 10, 11, 12, 13]
-indices_of_digit3 = [14, 15, 16, 17, 18, 19, 20]
-digit1 = []
-digit2 = []
-digit3 = []
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+binary_list_version = []
+new_binary_list = []
+correct_prods = []
 num_of_valid_nums = 0
 
-def check_for_amnt_of_differences(prod):
-    print(type(prod))
-    count = 0
-    for x, digit in prod:
-        if not digit == four_zero_three[x]:
-            count += 1
-    if count == 2:
-        return True
+def convert_to_binary_list(prod):
+    binary_list_version.clear()
+    for digit in prod:
+        binary_list_version.append(digit_to_list_dict[digit])
+    return binary_list_version
 
-def check_if_valid_num(digit1, digit2, digit3):
-    condition1 = False
-    condition2 = False
-    condition3 = False
-    for num in digit_to_list_dict:
-        if num == digit1:
-            condition1 = True
-        if num == digit2:
-            condition2 = True
-        if num == digit3:
-            condition3 = True
+def correct_amnt_differences(binary_list):
+    num_of_differences = 0
+    num_of_lights = 0
+    new_binary_list.clear()
+    for list_ in binary_list:
+        for item in list_:
+            new_binary_list.append(item)
     
-    if condition1 and condition2 and condition3:
+    for x, i in enumerate(new_binary_list):
+        if i != four_zero_three[x]:
+            num_of_differences += 1
+        if i == 1:
+            num_of_lights += 1
+    
+    if num_of_differences == 2 and num_of_lights == 15:
         return True
+    else:
+        return False
 
 
-for prod in list(product(digits, repeat=21)):
-    if check_for_amnt_of_differences(prod):
-        for x, digit in enumerate(prod):
-            if x in indices_of_digit1:
-                digit1.append(digit)
-            if x in indices_of_digit2:
-                digit2.append(digit)
-            if x in indices_of_digit3:
-                digit3.append(digit)
-        
-        if check_if_valid_num(digit1, digit2, digit3):
-            num_of_valid_nums += 1
+for prod in list(product(digits, repeat=3)):
+    if correct_amnt_differences(convert_to_binary_list(prod)):
+        num_of_valid_nums += 1
+        correct_prods.append(prod)
 
-print(num_of_valid_nums)
+print(f"Answer: {num_of_valid_nums}\nCorrect Products: {correct_prods}")
